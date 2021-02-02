17cd169dfc71afb6820fb0a35eb9358984483035
Rafał Filipek <rafal.filipek@orange.com>
2021-02-02 16:27:51 +0100

    clean version

--- dist/opl-ui.cjs.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./opl-ui.cjs.prod.js");
-} else {
-  module.exports = require("./opl-ui.cjs.dev.js");
-}
--- dist/opl-ui.cjs.prod.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', { value: true });
-
-const foo = 1;
-
-exports.foo = foo;
